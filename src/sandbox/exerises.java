f459f71336d9f2ca36c593ac39cffe299f52f8e9
andrewswint <andrewswint1976@gmail.com>
2020-03-02 11:17:12 -0600

    warm up exercises from edabit

--- src/sandbox/exerises.java
+++ src/sandbox/exerises.java
@@ -2,9 +2,18 @@ package sandbox;
 
 public class exerises {
 	
+	public static int addition(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int mod(int a, int b) {
+		return a % b;
+	}
+	
 	public static void main(String[] args) {
         
-		System.out.println("test");
+//		System.out.println(addition(2,3));
+		System.out.println(mod(3,4));
 		
     }
 	
