8faf8ba70ddc9f3c61510c7dcece129ae27b6b4e
andrewswint <andrewswint1976@gmail.com>
2020-03-06 13:42:35 -0600

    trying to figure the flip out still

--- src/sandbox/exerciese.java
+++ src/sandbox/exerciese.java
@@ -15,7 +15,7 @@ public class exerciese {
 }
 
 public static void main (String[] args) {
-	int[] array = {1,2,3,4,5};
+	
 	
 //	for (int i = 0; i < array.length; i ++) {
 //		System.out.println(array[array.length - i - 1]);
@@ -30,14 +30,26 @@ public static void main (String[] args) {
 	
 //	System.out.println(Arrays.toString(flip(array)));
 	
-	for (int i = 0; i < array.length - 1; i ++) {
-	      int a = array[i];
-	      int b = array[array.length -i];
-	      array[array.length - i] = a;
-	      array[i] = b;
-	      System.out.println(array[i]);
-	  }
-
+//	for (int i = 0; i < array.length - 1; i ++) {
+//	      int a = array[i];
+//	      int b = array[array.length -i -1];
+//	      array[array.length - i - 1] = a;
+//	      array[i] = b;
+//	      System.out.println(array[i]);
+//	  }
+//
+//	System.out.println(Arrays.toString(array));
+	
+	int[] array = {1,2,3,4,5};
+	
+	for (int i = 0, j = array.length - 1; i < array.length - 1 && j >= 0; i++, j--) {
+	int a = array[i];
+	int b = array[j];
+	array[i] = b;
+	array[j] = a;
+	}
+	
 	System.out.println(Arrays.toString(array));
+	
 }
 }
